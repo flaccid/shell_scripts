e889eddf376f218d70ab6c6ad1ef3ba6bb794e21
Chris Fordham <chris@xhost.com.au>
2011-07-10 15:51:36 +1000

    Change header sourcing.

--- rs_api_examples/rs-get-servers.sh
+++ rs_api_examples/rs-get-servers.sh
@@ -2,9 +2,10 @@
 
 # rs-get-servers
 
-. ./rs-login.sh
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
 
 url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers"
 echo "GET: $url"
-servers=$(curl -s -H "X-API-VERSION:$rs_api_version" -b "$rs_api_cookie" "$url")
+servers=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
 echo "$servers"
