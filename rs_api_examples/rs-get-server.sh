70b39ae35f73acac9e9108e620d6aa2610d4056e
Chris Fordham <chris@xhost.com.au>
2011-07-10 15:51:52 +1000

    Change header sourcing.

--- rs_api_examples/rs-get-server.sh
+++ rs_api_examples/rs-get-server.sh
@@ -4,7 +4,8 @@
 
 [[ ! $1 ]] && echo 'No server ID provided.' && exit 1
 
-. ./rs-login.sh
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
 
 if [ "$2" ]; then
     current="/$2"
@@ -12,5 +13,5 @@ fi
 
 url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers/$1$current"
 echo "GET: $url"
-server_xml=$(curl -s -H "X-API-VERSION:$rs_api_version" -b "$rs_api_cookie" "$url")
+server_xml=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
 echo "$server_xml"
