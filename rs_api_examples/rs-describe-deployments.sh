64843a191780edad0e02f132b63048f8dcddb23a
Chris Fordham <chris@xhost.com.au>
2011-07-10 15:52:28 +1000

    Re-factor design to standard args.

--- rs_api_examples/rs-describe-deployments.sh
+++ rs_api_examples/rs-describe-deployments.sh
@@ -1,18 +1,16 @@
 #!/bin/sh -e
 
-# rs-describe-deployments.sh
+# rs-describe-deployments.sh [settings]
 
-if [ "$2" = 'settings' ]; then
+if [ "$1" = 'settings' ]; then
     settings="/?server_settings=true"
 fi
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
-# get XML from API
 url="https://my.rightscale.com/api/acct/$rs_api_account_id/deployments$settings"
 echo "GET: $url"
-deployments_xml=$(curl -s -H "X_API_VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+api_result=$(curl -s -H "X_API_VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
 
-# print response from API
-echo "$deployments_xml"
+echo "$api_result"
