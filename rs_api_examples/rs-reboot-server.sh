470346ea748eb251830fec7581188765faa2aae8
Chris Fordham <chris@xhost.com.au>
2011-07-10 15:51:17 +1000

    Change header sourcing.

--- rs_api_examples/rs-reboot-server.sh
+++ rs_api_examples/rs-reboot-server.sh
@@ -2,9 +2,10 @@
 
 # rs-reboot-server
 
-. ./rs-login.sh
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
 
-[[ ! $1 ]] && echo 'No server ID provided.' && exit 1
+[[ ! $1 ]] && echo 'No server ID provided, exiting.' && exit 1
 server_id="$1"
 
 url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers/$server_id/reboot"
